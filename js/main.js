42c0d259b4521b46873d6492d1772df022ab7da6
chrisnnaji443@gmail.com <chrisnnaji443@gmail.com>
2022-07-27 22:35:59 +0100

    commit

---
+++ js/main.js
@@ -0,0 +1,130 @@
+let btn1=document.querySelector("#btn1");
+let btn3=document.querySelector("#btn3");
+let btn2=document.querySelector("#btn2");
+let btn4=document.querySelector("#btn4");
+
+
+let idn1=document.querySelector("#idn1");
+let idn2=document.querySelector("#idn2");
+let idn3=document.querySelector("#idn3");
+let idn4=document.querySelector("#idn4");
+let idn5=document.querySelector("#idn5");
+let idn6=document.querySelector("#idn6");
+
+
+let dn1=document.querySelector("#dn1");
+let dn2=document.querySelector("#dn2");
+let dn3=document.querySelector("#dn3");
+let dn4=document.querySelector("#dn4");
+let dn5=document.querySelector("#dn5");
+let dn6=document.querySelector("#dn6");
+
+
+
+
+let da1=document.querySelector("#dw1");
+let da2=document.querySelector("#dw2");
+let da3=document.querySelector("#dw3");
+let da4=document.querySelector("#dw4");
+let da5=document.querySelector("#dw5");
+let da6=document.querySelector("#dw6");
+
+
+let ida1=document.querySelector("#ida1");
+let ida2=document.querySelector("#ida2");
+let ida3=document.querySelector("#ida3");
+let ida4=document.querySelector("#ida4");
+let ida5=document.querySelector("#ida5");
+let ida6=document.querySelector("#ida6");
+
+
+
+
+
+let iwn1=document.querySelector("#iwn1");
+let iwn2=document.querySelector("#iwn2");
+let iwn3=document.querySelector("#iwn3");
+let iwn4=document.querySelector("#iwn4");
+let iwn5=document.querySelector("#iwn5");
+let iwn6=document.querySelector("#iwn6");
+
+
+
+let iwa1=document.querySelector("#iwa1");
+let iwa2=document.querySelector("#iwa2");
+let iwa3=document.querySelector("#iwa3");
+let iwa4=document.querySelector("#iwa4");
+let iwa5=document.querySelector("#iwa5");
+let iwa6=document.querySelector("#iwa6");
+
+
+
+
+let wa1=document.querySelector("#wa1");
+let wa2=document.querySelector("#wa2");
+let wa3=document.querySelector("#wa3");
+let wa4=document.querySelector("#wa4");
+let wa5=document.querySelector("#wa5");
+let wa6=document.querySelector("#wa6");
+
+
+let wn1=document.querySelector("#wn1");
+let wn2=document.querySelector("#wn2");
+let wn3=document.querySelector("#wn3");
+let wn4=document.querySelector("#wn4");
+let wn5=document.querySelector("#wn5");
+let wn6=document.querySelector("#wn6");
+
+
+
+
+btn1.addEventListener("click",()=>{
+
+   dn1.innerHTML=idn1.value + "**";
+   dn2.innerHTML=idn2.value + "**";
+   dn3.innerHTML=idn3.value + "**";
+   dn4.innerHTML=idn4.value + "**";
+   dn5.innerHTML=idn5.value + "**";
+   dn6.innerHTML=idn6.value + "**";
+   dn7.innerHTML=idn7.value + "**"; 
+   dn8.innerHTML=idn8.value + "**";
+
+  
+
+
+})
+
+btn3.addEventListener("click",()=>{
+    da1.innerHTML= "$" + ida1.value;
+    da2.innerHTML= "$" + ida2.value;
+    da3.innerHTML= "$" + ida3.value;
+    da4.innerHTML= "$" + ida4.value;
+    da5.innerHTML= "$" + ida5.value;
+    da6.innerHTML= "$" + ida6.value;
+    da7.innerHTML= "$" + ida7.value; 
+    da8.innerHTML= "$" + ida8.value;
+})
+
+btn4.addEventListener("click",()=>{
+    wa1.innerHTML= "$" + iwa1.value;
+    wa2.innerHTML= "$" + iwa2.value;
+    wa3.innerHTML= "$" + iwa3.value;
+    wa4.innerHTML= "$" + iwa4.value;
+    wa5.innerHTML= "$" + iwa5.value;
+    wa6.innerHTML= "$" + iwa6.value;
+    wa7.innerHTML= "$" + iwa7.value; 
+    wa8.innerHTML= "$" + iwa8.value;
+})
+
+btn2.addEventListener("click",()=>{
+
+    wn1.innerHTML=iwn1.value + "**";
+    wn2.innerHTML=iwn2.value + "**";
+    wn3.innerHTML=iwn3.value + "**";
+    wn4.innerHTML=iwn4.value + "**";
+    wn5.innerHTML=iwn5.value + "**";
+    wn6.innerHTML=iwn6.value + "**";
+ 
+ 
+   
+ })
